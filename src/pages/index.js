49247290b8cbabf38eb42f0464b330e4fafa75a1
David Li <david.li3100@gmail.com>
2020-03-18 22:09:06 -0400

    update: added a TODO to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,11 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Login from "../components/Login"
 
+/**
+ * TODO: just make the index page the login page
+ */
 const IndexPage = () => (
   <Layout>
-    {/* <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> */}
     <Login/>
   </Layout>
 )
