e3b2c2a7a8ca120b69e2234769722f2e3960a5e5
David Li <david.li3100@gmail.com>
2020-03-24 22:53:21 -0400

    chore: removed unused dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/Layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import Login from "../components/Login"
-import { ThemeProvider, LightMode } from "@chakra-ui/core"
-import theme from "../gatsby-plugin-chakra-ui/theme"
+import { LightMode } from "@chakra-ui/core"
 
 /**
  * TODO: just make the index page the login page
