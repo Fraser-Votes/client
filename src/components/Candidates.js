c99773d6dbe196ddef4a1bf47971a23f193e04e4
David Li <david.li3100@gmail.com>
2020-03-25 21:12:38 -0400

    test deploy to gatsby

--- src/components/Candidates.js
+++ src/components/Candidates.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import { Text, Box } from '@chakra-ui/core'
 import Header from "./Header"
 import Layout from './Layout'
+import firebase from "gatsby-plugin-firebase"
 
-const CandidateCard = ({name, grade, position, id}) => {
-        
-}
+const db = firebase.firestore()
+const candidateRef = db.collection("candidates")
 
 const CandidateRow = ({position, candidates}) => {
     return (
@@ -18,14 +18,59 @@ const CandidateRow = ({position, candidates}) => {
         </Text>
     )
 }
+
 export default class Candidates extends Component {
+
+    constuctor(props) {
+        super(props)
+
+        this.state = {
+            candidates: null,
+            dataLoading: false
+        }
+
+    }
+
+    async componentDidMount() {
+        const candidates = await getCandidates()
+        this.setState({
+            candidates: candidates
+        })
+        console.log(this.state.candidates)
+    }
+
+    createCandidateRows = () => {
+        this.state.candidates.forEach(position => {
+            return <CandidateRow position={position.key()}/>
+        })
+    }
+
 
     render() {
         return (
             <Layout>    
                 <Header title="Candidates"/>
-                <CandidateRow position="President" />
+                {this.createCandidateRows()}
             </Layout>
         )
     }
+}
+
+const getCandidates = () => {
+
+    var candidates = {}
+
+    db.collection("positions").get().then(res => {
+        res.forEach(position => {
+            candidates[position.id] = []
+            var positionDocRef = db.collection("positions").doc(position.id)
+            candidateRef.where("position", "==", positionDocRef).get().then(res => {
+                res.forEach(doc => {
+                    candidates[position.id].push(doc.data())
+                })
+            }).catch(err => console.log(err)) 
+        })
+    })
+
+    return candidates
 }
