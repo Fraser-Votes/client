e4f85311ef0e1db6b1f6e4332c85c9dea664aad3
David Li <david.li3100@gmail.com>
2020-04-06 21:58:45 -0400

    Add a quick 404 page

--- src/pages/app.js
+++ src/pages/app.js
@@ -5,11 +5,13 @@ import Login from "../components/Login"
 import { LightMode, Box } from "@chakra-ui/core"
 import Candidates from "../components/Candidates"
 import Voting from "../components/Voting"
+import NotFoundPage from "./404"
 
 const App = () => (
   <LightMode>
   <Box>
     <Router basepath="/app">
+        <NotFoundPage default/>
         <PrivateRoute path="/candidates" component={Candidates} />
         <PrivateRoute path="/voting" component={Voting} />
         <PrivateRoute path="/results" component={Candidates} />
