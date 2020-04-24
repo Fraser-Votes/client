e11f5eaac715c45644067bf34bc055a0ead1fce2
David Li <david.li3100@gmail.com>
2020-04-23 22:10:30 -0400

    update SVG Path props

--- static/icons/icons.js
+++ static/icons/icons.js
@@ -5,17 +5,17 @@ export default {
         path: <path d="M18.5 19.667h5.833v-2.334a3.5 3.5 0 0 0-6.248-2.166m.415 4.5l-.415-4.5m.415 4.5H6.833m11.667 0v-2.334c0-.765-.147-1.496-.415-2.166m0 0a5.836 5.836 0 0 0-10.836 0m10.836 0H7.249m-.416 4.5H1v-2.334a3.5 3.5 0 0 1 6.249-2.166m-.416 4.5l.416-4.5m-.416 4.5v-2.334c0-.765.147-1.496.416-2.166M16.167 4.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0zm7 3.5A2.333 2.333 0 1 1 18.5 8a2.333 2.333 0 0 1 4.667 0zM6.833 8a2.333 2.333 0 1 1-4.666 0 2.333 2.333 0 0 1 4.666 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style={{fill: "transparent"}}/>,
     },
     voting: {
-        path: <path d="M7 10l2 2 4-4m6 2a9.0004 9.0004 0 0 1-5.5558 8.3149A9.0004 9.0004 0 0 1 3.636 16.364 9.0011 9.0011 0 0 1 1 10a9 9 0 1 1 18 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style={{fill: "transparent"}}/>
+        path: <path d="M7 10l2 2 4-4m6 2a9.0004 9.0004 0 0 1-5.5558 8.3149A9.0004 9.0004 0 0 1 3.636 16.364 9.0011 9.0011 0 0 1 1 10a9 9 0 1 1 18 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{fill: "transparent"}}/>
     },
     results: {
-        path: <path d="M8 9h4M3 5h14H3zm0 0a2 2 0 1 1 0-4h14a2 2 0 0 1 0 4H3zm0 0v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V5H3z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style={{fill: "transparent"}}/>
+        path: <path d="M8 9h4M3 5h14H3zm0 0a2 2 0 1 1 0-4h14a2 2 0 0 1 0 4H3zm0 0v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V5H3z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{fill: "transparent"}}/>
     },
     forward: {
-        path: <path d="M13 3.99998H1M10.3333 1.33331L13 3.99998L10.3333 1.33331ZM13 3.99998L10.3333 6.66665L13 3.99998Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style={{fill: "transparent"}}/>,
+        path: <path d="M13 3.99998H1M10.3333 1.33331L13 3.99998L10.3333 1.33331ZM13 3.99998L10.3333 6.66665L13 3.99998Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{fill: "transparent"}}/>,
         viewBox: "0 0 14 8"
     },
     back: {
-        path: <path d="M8.5 14.4922L1 8.10639M1 8.10639L8.5 1.72058M1 8.10639H23.375" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>,
+        path: <path d="M8.5 14.4922L1 8.10639M1 8.10639L8.5 1.72058M1 8.10639H23.375" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>,
         viewBox: "0 0 25 16"
     },
     email: {
@@ -35,7 +35,7 @@ export default {
         viewBox: "0 0 10 19"
     },
     menu: {
-        path: <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" fill="currentColor"/>,
+        path: <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" fill="currentColor"/>,
         viewbox: "0 0 20 20"
     }
 }
