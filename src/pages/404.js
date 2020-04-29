2ba937cc544cda714279d6725141d2d527441f23
David Li <david.li3100@gmail.com>
2020-04-29 13:10:52 -0400

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,9 @@
 import React from "react"
+import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <div className="404-wrapper">
+    <SEO title="404" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
