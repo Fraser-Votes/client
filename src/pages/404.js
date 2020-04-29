86b0c29bbd38ca0ed42b217b46e2b5e75b53fafb
David Li <david.li3100@gmail.com>
2020-04-29 13:04:35 -0400

    remove 404 SEO

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,6 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <div className="404-wrapper">
-    <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
--- src/pages/app.js
+++ src/pages/app.js
@@ -5,7 +5,6 @@ import Login from "../components/Login"
 import { LightMode, Box } from "@chakra-ui/core"
 import Candidates from "../components/Candidates"
 import Voting from "../components/Voting"
-import NotFoundPage from "./404"
 import Profile from  '../components/Profile/Profile'
 import Results from "../components/Results"
 
@@ -18,7 +17,6 @@ const App = () => (
         <PrivateRoute path="results" component={Results} />
         <PrivateRoute path="candidates/:candidateID" component={Profile}/>
         <Login path="login" />
-        {/* <NotFoundPage default/> */}
     </Router>
   </Box>
   </LightMode>
