c6731c2d0822a75537dbd4fea772ad749f106ed7
RetroCraft <retrocraft314@gmail.com>
2020-05-04 23:40:27 -0400

    test hack for 404-flash avoidance

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,12 @@
 import React from "react"
 import SEO from "../components/seo"
 
+const browser = typeof window !== "undefined" && window
 const NotFoundPage = () => (
-  <div className="404-wrapper">
+  browser && (<div className="404-wrapper">
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  </div>)
 )
 
 export default NotFoundPage
