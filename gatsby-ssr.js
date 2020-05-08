1147f4ae71ae70117b7b79d46a2799c755254f1f
David Li <david.li3100@gmail.com>
2020-05-08 19:34:31 -0400

    remove chatbox

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,31 +11,3 @@ import 'firebase/firestore';
 import 'firebase/functions';
 import 'firebase/storage';
 import React from 'react';
-
-export const onRenderBody = ({ pathname, setPostBodyComponents }) => {
-        setPostBodyComponents([
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    function initFreshChat() {
-                        window.fcWidget.init({
-                        token: "05d686db-6ad6-41fb-b3a1-86a669df5b8c",
-                        host: "https://wchat.freshchat.com"
-                        });
-                    }
-                    function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
-                    `
-                }}
-            />
-             // <script defer data-cfasync="false" type="text/javascript"
-            //     dangerouslySetInnerHTML={{
-            //         __html: `
-            //             window.civchat = {
-            //                 apiKey: "eWKGsg",
-            //             };
-            //             `
-            //     }}
-            // />,
-            // <script defer data-cfasync="false" type="text/javascript" src="https://fraservotes.user.com/widget.js"></script>
-        ])
-}
