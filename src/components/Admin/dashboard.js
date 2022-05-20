f674584dd570e40e829d9ba383dc15c30c78a1b5
David Li <david.li3100@gmail.com>
2022-05-19 20:14:47 -0400

    Update dashboard.js

--- src/components/Admin/dashboard.js
+++ src/components/Admin/dashboard.js
@@ -245,7 +245,6 @@ export default class Dashboard extends Component {
       <Layout>
         <AdminSEO title="Dashboard" />
       <iframe plausible-embed src="https://plausible.io/share/fraservotes.com?auth=cWmhHJ0J6FeneC6USBpZM&embed=true&theme=light&background=transparent" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
-<div style="font-size: 14px; padding-bottom: 14px;">Stats powered by <a target="_blank" style="color: #4F46E5; text-decoration: underline;" href="https://plausible.io">Plausible Analytics</a></div>
 <script async src="https://plausible.io/js/embed.host.js"></script>
       {/*
         {this.state.loading1
