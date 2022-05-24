47aba0098afb7d7aaed0bd41c939ad4c6812cfb3
David Li <david.li3100@gmail.com>
2022-05-24 15:00:08 -0400

    oops

--- src/components/Admin/newDashboard.js
+++ src/components/Admin/newDashboard.js
@@ -9,7 +9,7 @@ const NewDashboard = () => (
       <script async src="https://plausible.io/js/embed.host.js"></script>
     </Helmet>
     <AdminSEO title="Dashboard" />
-    <iframe plausible-embed src="https://plausible.io/share/fraservotes.com?auth=cWmhHJ0J6FeneC6USBpZM&embed=true&theme=light&background=transparent" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+    <iframe plausible-embed src="https://plausible.io/share/fraservotes.com?auth=cWmhHJ0J6FeneC6USBpZM&embed=true&theme=light&background=transparent" scrolling="no" frameborder="0" loading="lazy" style={{width: '1px', minWidth: '100%', height: '1600px' }}></iframe>
   </Layout>
 );
 
